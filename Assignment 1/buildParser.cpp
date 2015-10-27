40b02ecd507bfbb5b2a73a0107d8f52a7735b0fa
dougfultz <dougfultz@users.noreply.github.com>
2015-10-27 14:05:18 -0400

    buildParser.cpp: Added support to open file from command line parameter.

--- Assignment 1/buildParser.cpp
+++ Assignment 1/buildParser.cpp
@@ -1,13 +1,33 @@
 /** Doug Fultz
  *  CMPSC470 - Assignment 1
  *  Dr. El Ariss
-    buildParser.cpp
+ *  buildParser.cpp
  */
 
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 //===================================================================
 int main(int argc, char * argv[]){
     cout<<"Hello World"<<endl;
+    
+    //Accept file as only argument.
+    //http://www.cprogramming.com/tutorial/lesson14.html
+    //http://www.cplusplus.com/articles/DEN36Up4/
+    if(argc!=2){
+        cerr<<"Usage: "<<argv[0]<<" FILE"<<endl;
+        cerr<<endl;
+        cerr<<"    FILE - Path to file containing parse table and CFG"<<endl;
+        return(1);
+    }else{
+        ifstream inputFile(argv[1]);
+        if(!inputFile.is_open()){
+            cerr<<"Could not open file: "<<argv[1]<<endl;
+            return(1);
+        }else{
+            cout<<"File opened."<<endl;
+        }
+        inputFile.close();
+    }
 }
