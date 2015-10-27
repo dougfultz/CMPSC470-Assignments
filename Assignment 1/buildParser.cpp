a2fd3892d64f71ad890d9f54ec362b52e0c29f3d
dougfultz <dougfultz@users.noreply.github.com>
2015-10-27 12:17:45 -0400

    buildParser.cpp: initial commit

---
+++ Assignment 1/buildParser.cpp
@@ -0,0 +1,13 @@
+/** Doug Fultz
+ *  CMPSC470 - Assignment 1
+ *  Dr. El Ariss
+    buildParser.cpp
+ */
+
+#include <iostream>
+
+using namespace std;
+//===================================================================
+int main(int argc, char * argv[]){
+    cout<<"Hello World"<<endl;
+}
