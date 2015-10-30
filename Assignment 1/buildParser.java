1183954e18e249f18e0b44bcfc7819d06c4d19a7
dougfultz <dougfultz@users.noreply.github.com>
2015-10-30 15:35:06 -0400

    buildParser.java: Read entire file into string

--- Assignment 1/buildParser.java
+++ Assignment 1/buildParser.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.*;
 
 /** buildParser.java
  *  @author Doug Fultz
@@ -10,6 +11,15 @@ class buildParser{
     public static void main(String args[]) throws java.io.IOException {
         if(args.length!=1){
             System.err.println("Error: Input file must be named on command line.");
+            System.exit(-1);
+        }
+        
+        try{
+            //http://abhinandanmk.blogspot.com/2012/05/java-how-to-read-complete-text-file.html
+            String inputString = new Scanner(new File(args[0])).useDelimiter("\\A").next();
+            System.out.println(inputString);
+        }catch(FileNotFoundException notFound){
+            System.err.println("Error: file not found: "+args[0]);
             System.exit(-1);
         }
     } //main()
