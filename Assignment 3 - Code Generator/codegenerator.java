6144f4fcec7936697b8f8add7fec419f9fc805c8
dougfultz <dougfultz@users.noreply.github.com>
2015-12-06 20:42:07 -0500

    Output instructions and begin reading scores from user

--- Assignment 3 - Code Generator/codegenerator.java
+++ Assignment 3 - Code Generator/codegenerator.java
@@ -5,6 +5,9 @@ import java.util.*;
  *  @author Doug Fultz
  */
 class codegenerator{
+    private static String enterScore = "Enter score: ";
+    private static String strNoScore = "ERROR: no score provided.";
+    
     private static final String jasminJarPath="./jasmin.jar";
     private static final String outputFilePath="./simple.j";
     
@@ -29,10 +32,43 @@ class codegenerator{
         printToFile.println();
     }
     
+    /** Reads test scores provided by user from console
+     *  Input will be stored in class vector
+     */
+    private static void readScores(){
+        //https://docs.oracle.com/javase/tutorial/essential/io/cl.html
+        Console c = System.console();
+        if (c == null) {
+            System.err.println("No console.");
+            System.exit(1);
+        }
+        
+        String input;
+        //https://docs.oracle.com/javase/tutorial/java/nutsandbolts/while.html
+        do{
+            //Read input from user
+            input = c.readLine(enterScore);
+            
+            //Check if input is empty
+            if(input.isEmpty()){
+                System.out.println(strNoScore);
+                continue;
+            }
+        }while(!input.equals("-9"));
+    }
+    
     /** MAIN function
      *  Starts program
      */
     public static void main(String args[]) throws java.io.IOException {
+        //Output instructions to user
+        System.out.println("Enter test scores one at a time.");
+        System.out.println("Score of \"-9\" is end of input.");
+        System.out.println();
+        
+        //Read scores
+        readScores();
+        
         //Prepare output file
         try{
             //http://www.homeandlearn.co.uk/java/write_to_textfile.html
