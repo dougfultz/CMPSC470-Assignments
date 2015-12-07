898670a1c0fa0c2f3807f3878e15f884e5fb8081
dougfultz <dougfultz@users.noreply.github.com>
2015-12-06 20:27:49 -0500

    outputHead(): place initial head of output

--- Assignment 3 - Code Generator/codegenerator.java
+++ Assignment 3 - Code Generator/codegenerator.java
@@ -11,6 +11,24 @@ class codegenerator{
     private static FileWriter outputFile;
     private static PrintWriter printToFile;
     
+    /** Writes the beginning of the class
+     *  Content will be printed as it appears to the output file
+     */
+    private static void outputHead(){
+        printToFile.println(".class simple");
+        printToFile.println(".super java/lang/Object");
+        printToFile.println(".method public <init>()V");
+        printToFile.println("    .limit stack 1");
+        printToFile.println("    .limit locals 1");
+        printToFile.println("    aload_0");
+        printToFile.println("    invokespecial java/lang/Object/<init>()V");
+        printToFile.println("    return");
+        printToFile.println(".end method");
+        printToFile.println();
+        printToFile.println();
+        printToFile.println();
+    }
+    
     /** MAIN function
      *  Starts program
      */
@@ -24,6 +42,9 @@ class codegenerator{
             System.err.println("Error: unable to open file for output: "+outputFilePath);
             System.exit(-1);
         }
+        
+        //Place beginning of class structure
+        outputHead();
         
         //Final newline
         printToFile.println();
